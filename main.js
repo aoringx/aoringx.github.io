a81a2e57c010bc73928f08a1440590560f015707
Aaron <aaronxiong.2004@gmail.com>
2024-07-16 15:22:15 +0800

    improved coding style, changed achievements to resume

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 const buttonHome = document.getElementById("button_home");
 const buttonEducation = document.getElementById("button_education");
 const buttonExperience = document.getElementById("button_experience");
-const buttonAchievements = document.getElementById("button_achievements");
+const buttonResume = document.getElementById("button_resume");
 
 const pageHome = document.getElementById("page_home");
 const pageEducation = document.getElementById("page_education");
 const pageExperience = document.getElementById("page_experience");
-const pageAchievements = document.getElementById("page_achievements");
+const pageResume = document.getElementById("page_resume");
 
 buttonHome.addEventListener("click", goToHome);
 buttonEducation.addEventListener("click", goToEducation);
 buttonExperience.addEventListener("click", goToExperience);
-buttonAchievements.addEventListener("click", goToAchievements);
+buttonResume.addEventListener("click", goToResume);
 
 function goToHome(){
     pageHome.style.display = "block";
     pageEducation.style.display = "none";
     pageExperience.style.display = "none";
-    pageAchievements.style.display = "none";
+    pageResume.style.display = "none";
 }
 
 function goToEducation(){
     pageHome.style.display = "none";
     pageEducation.style.display = "block";
     pageExperience.style.display = "none";
-    pageAchievements.style.display = "none";
+    pageResume.style.display = "none";
 }
 
 function goToExperience(){
     pageHome.style.display = "none";
     pageEducation.style.display = "none";
     pageExperience.style.display = "block";
-    pageAchievements.style.display = "none";
+    pageResume.style.display = "none";
 }
 
-function goToAchievements(){
+function goToResume(){
     pageHome.style.display = "none";
     pageEducation.style.display = "none";
     pageExperience.style.display = "none";
-    pageAchievements.style.display = "block";
+    pageResume.style.display = "block";
 }
