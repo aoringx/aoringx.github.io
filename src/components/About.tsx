dfd4ec6d39714dd892f7defb12b785b4193e7211
Aaron <aaronxiong.2004@gmail.com>
2025-09-05 00:20:29 -0400

    fixed typo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,95 +1,106 @@
 import React from "react";
-import '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact } from '@fortawesome/free-brands-svg-icons';
-import { faBrain , faRobot} from '@fortawesome/free-solid-svg-icons';
-import Chip from '@mui/material/Chip';
-import '../assets/styles/About.scss';
+import "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faBrain, faRobot } from "@fortawesome/free-solid-svg-icons";
+import Chip from "@mui/material/Chip";
+import "../assets/styles/About.scss";
 
-const labelsFirst = [
-    "Python",
-    "NLP",
-    "Gemini",
-    "Hugging Face"
-];
+const labelsFirst = ["Python", "NLP Research", "Gemini", "Hugging Face"];
 
 const labelsSecond = [
-    "React",
-    "Tailwind CSS",
-    "Bootstrap",
-    "Flask",
-    "Typescript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Python",
-    "Git"
+  "React",
+  "Tailwind CSS",
+  "Bootstrap",
+  "Flask",
+  "Typescript",
+  "JavaScript",
+  "HTML5",
+  "CSS3",
+  "SASS",
+  "Python",
+  "Git",
+  "Docker",
 ];
 
 const labelsThird = [
-    "Embedded C",
-    "Arduino",
-    "Verilog",
-    "FPGA",
-    "NIOS II",
+  "ARMv7",
+  "Embedded C",
+  "Arduino",
+  "Verilog",
+  "FPGA",
+  "NIOS II",
 ];
 
 function About() {
-    return (
+  return (
     <div className="container" id="about">
-        <div className="skills-container">
-            <h1>About</h1>
-            <p className="about">
-                Welcome to my home page! <br></br>
-                I am a senior undergraduate student majoring in Computer Science at the University of Michigan. <br></br>
-                My research interest is in Machine Learning and Natural Language Processing, and I currently serve as a research assistant at the LAUNCH Lab. 
-                I am also passionate about embedded systems and computer security. <br></br>
-                I am fluent in C++ and Python, with proficiency in web development. <br></br>
-                In my free time, I enjoy swimming, badminton, dancing, and singing.
+      <div className="skills-container">
+        <h1>About</h1>
+        <p className="about">
+          Welcome to my home page! <br></br>I am a senior undergraduate student
+          majoring in Computer Science at the University of Michigan. <br></br>
+          My research interest is in Machine Learning and Natural Language
+          Processing, and I currently serve as a research assistant at the
+          LAUNCH Lab. I am also passionate about embedded systems and computer
+          security. <br></br>I am fluent in C++ and Python, and proficient in
+          web development. <br></br>
+          In my free time, I enjoy swimming, badminton, dancing, and singing.
+        </p>
+
+        <div className="skills-grid">
+          <div className="skill">
+            <FontAwesomeIcon icon={faBrain} size="3x" />
+            <h3>Large Language Model Evaluation</h3>
+            <p>
+              I parsed competitive-level coding problems, evaluated LLM
+              performances against contestants, and created an open-source
+              benchmark for LLM training.
             </p>
-
-            <div className="skills-grid">
-
-                <div className="skill">
-                    <FontAwesomeIcon icon={faBrain} size="3x"/>
-                    <h3>Large Language Model Evaluation</h3>
-                    <p>I parsed competitive-level coding problems, evaluated LLM performances against contestants, and created a open-source benchmark for LLM training.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsFirst.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full-stack Web Development</h3>
-                    <p>I have developed full-stack web applications with modern libraries and frameworks including React and Flask. I also have experience with incorporating LLM responses, video conferencing, secure data storage and geospatial mapping, etc.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsSecond.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className="skill">
-                    <FontAwesomeIcon icon={faRobot} size="3x"/>
-                    <h3>Embeddded Stystem Design</h3>
-                    <p>I designed embedded video games on FPGA boards, implementing display, controls, and sound functionalities. I also programmed autonomous Arduino robots</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsThird.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
+            <div className="flex-chips">
+              <span className="chip-title">Tech stack:</span>
+              {labelsFirst.map((label, index) => (
+                <Chip key={index} className="chip" label={label} />
+              ))}
             </div>
+          </div>
+
+          <div className="skill">
+            <FontAwesomeIcon icon={faReact} size="3x" />
+            <h3>Full-stack Web Development</h3>
+            <p>
+              I have developed full-stack web applications with modern libraries
+              and frameworks including React and Flask. I also have experience
+              with incorporating LLM responses, video conferencing, secure data
+              storage and geospatial mapping, etc.
+            </p>
+            <div className="flex-chips">
+              <span className="chip-title">Tech stack:</span>
+              {labelsSecond.map((label, index) => (
+                <Chip key={index} className="chip" label={label} />
+              ))}
+            </div>
+          </div>
+
+          <div className="skill">
+            <FontAwesomeIcon icon={faRobot} size="3x" />
+            <h3>Embedded System Design</h3>
+            <p>
+              I designed embedded video games on FPGA boards, implementing
+              display, controls, and sound functionalities. I also programmed
+              autonomous Arduino robots
+            </p>
+            <div className="flex-chips">
+              <span className="chip-title">Tech stack:</span>
+              {labelsThird.map((label, index) => (
+                <Chip key={index} className="chip" label={label} />
+              ))}
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default About;
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,39 +1,78 @@
 import React from "react";
-import '../assets/styles/Project.scss';
+import "../assets/styles/Project.scss";
 
 function Project() {
-    return(
+  return (
     <div className="projects-container" id="projects">
-        <h1>Projects</h1>
-        <div className="projects-grid">
-
-            <div className="project">
-                {/* <a href="https://github.com/AoringX/100-TheHive" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
-                <a href="https://github.com/AoringX/100-TheHive" target="_blank" rel="noreferrer"><h2>The Hivebot</h2></a>
-                <p>Designed an Arduino-based autonomous robot that can parse CSV blueprints, communicate with serial ports through PySerial, and collaboratively perform transportation tasks on a grid.</p>
-            </div>
-            
-            <div className="project">
-                {/* <a href="https://github.com/AoringX/298-MLClassifier" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
-                <a href="https://github.com/AoringX/298-MLClassifier" target="_blank" rel="noreferrer"><h2>Fake News Classifier</h2></a>
-                <p>Implemented a Naive Bayes Classifier in C++ to identify misinformation in news articles using a training dataset of 12,000+ articles. Created a preprocessing pipeline with text normalization, N-gram modeling, and Word Stemming to reach an accuracy of 83.8%. </p>
-            </div>
-            
-            <div className="project">
-                {/* <a href="https://github.com/Edwardobird/ECE297Project" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
-                <a href="https://github.com/Edwardobird/ECE297Project" target="_blank" rel="noreferrer"><h2>EVie</h2></a>
-                <p>Developed a Geographic Information System in C++ for electric vehicle users across multiple regions by extracting real-world map data from StreetsDatabase and OpenStreetMapDatabase.</p>
-            </div>
-            
-            <div className="project">
-                {/* <a href="https://github.com/AoringX/243-SuperYoshi" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
-                <a href="https://github.com/AoringX/243-SuperYoshi" target="_blank" rel="noreferrer"><h2>Super Yoshi</h2></a>
-                <p>Created a voice-controlled 2D plaformer Game in Embedded C on a DE1-SoC FPGA board with double-buffered VGA display and PS/2 keyboard controls.</p>
-            </div>
-
+      <h1>Projects</h1>
+      <div className="projects-grid">
+        <div className="project">
+          {/* <a href="https://github.com/AoringX/100-TheHive" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
+          <a
+            href="https://github.com/AoringX/100-TheHive"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2>The Hivebot</h2>
+          </a>
+          <p>
+            Designed an Arduino-based autonomous robot that can parse CSV
+            blueprints, communicate with serial ports through PySerial, and
+            collaboratively perform transportation tasks on a grid.
+          </p>
         </div>
+
+        <div className="project">
+          {/* <a href="https://github.com/AoringX/298-MLClassifier" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
+          <a
+            href="https://github.com/AoringX/298-MLClassifier"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2>Fake News Classifier</h2>
+          </a>
+          <p>
+            Implemented a Naive Bayes Classifier in C++ to identify
+            misinformation in news articles using a training dataset of 12,000+
+            articles. Created a preprocessing pipeline with text normalization,
+            N-gram modeling, and Word Stemming to reach an accuracy of 83.8%.{" "}
+          </p>
+        </div>
+
+        <div className="project">
+          {/* <a href="https://github.com/Edwardobird/ECE297Project" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
+          <a
+            href="https://github.com/Edwardobird/ECE297Project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2>EVie</h2>
+          </a>
+          <p>
+            Developed a Geographic Information System in C++ for electric
+            vehicle users across multiple regions by extracting real-world map
+            data from StreetsDatabase and OpenStreetMapDatabase.
+          </p>
+        </div>
+
+        <div className="project">
+          {/* <a href="https://github.com/AoringX/243-SuperYoshi" target="_blank" rel="noreferrer"><img src={} className="zoom" alt="thumbnail" width="100%"/></a> */}
+          <a
+            href="https://github.com/AoringX/243-SuperYoshi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2>Super Yoshi</h2>
+          </a>
+          <p>
+            Created a voice-controlled 2D platformer Game in Embedded C on a
+            DE1-SoC FPGA board with double-buffered VGA display and PS/2
+            keyboard controls.
+          </p>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Project;
