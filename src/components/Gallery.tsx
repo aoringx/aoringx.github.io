354efc517f57ce491b363855e2081138dcbd0c2b
Aaron <aaronxiong.2004@gmail.com>
2026-02-23 02:18:58 -0500

    another pic

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,8 +4,9 @@ import photo1 from "../assets/images/gallery/1.jpeg";
 import photo2 from "../assets/images/gallery/2.jpeg";
 import photo3 from "../assets/images/gallery/3.jpeg";
 import photo4 from "../assets/images/gallery/4.jpeg";
+import photo5 from "../assets/images/gallery/5.jpeg";
 
-const images: string[] = [photo1, photo2, photo3, photo4];
+const images: string[] = [photo1, photo2, photo3, photo4, photo5];
 
 function Gallery() {
   return (
